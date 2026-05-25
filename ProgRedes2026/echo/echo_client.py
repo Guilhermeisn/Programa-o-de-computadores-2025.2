db487948a8d3bca3314cd8286ff2623bfd7a8791
Guilhermeisn <guilherme331b@gmail.com>
2026-05-25 20:26:44 -0300

    Aula 25052026

--- ProgRedes2026/echo/echo_client.py
+++ ProgRedes2026/echo/echo_client.py
@@ -3,12 +3,11 @@ from config import *
 
 my_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_ip = input("IP/nome do servidor:")
 while True:
     msg = input("Mensagem: ").encode()
     if msg:
         print (f"Enviando: {msg}")
-        my_sock.sendto(msg, (server_ip, PORT))
+        my_sock.sendto(msg, (SERVER, PORT))
         answer, source = my_sock.recvfrom(512)
         print (f"Recebido de {source}: {answer}")
     else:
