38b1262c7745bcc33aa7e665bee971a5abe1522f
Guilhermeisn <guilherme331b@gmail.com>
2026-06-20 18:25:24 -0300

    Desenvolvimento client TCP

--- Atividade client e server TCP/TCPclient.py
+++ Atividade client e server TCP/TCPclient.py
@@ -1,3 +1,11 @@
 import socket
 import sys
 
+if len(sys.argv) != 4:
+    print("Uso: python TCPclient.py host porta mensagem")
+    sys.exit()
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+MENSAGEM = sys.argv[3]
+
