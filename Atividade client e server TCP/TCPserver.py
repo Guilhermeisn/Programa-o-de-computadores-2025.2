e6b1b77305576b7a4728e4d3ef18e6ae7b8c953c
Guilhermeisn <guilherme331b@gmail.com>
2026-06-20 17:52:38 -0300

    Contnuação TCPserver

--- Atividade client e server TCP/TCPserver.py
+++ Atividade client e server TCP/TCPserver.py
@@ -1,2 +1,17 @@
 import socket
 import sys
+
+HOST = "0.0.0.0"
+
+if len(sys.argv) != 2:
+    print("Uso: python TCPserver.py porta")
+    sys.exit()
+
+PORT = int(sys.argv[1])
+
+server = socket.socket(socket.AF_INET, socket.SCOCK_STREAM)
+
+server.bind((HOST, PORT))
+server.listen(5)
+
+print(f"Servidor ouvindo. Porta: {PORT}")
